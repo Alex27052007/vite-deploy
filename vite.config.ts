bd81c9c95b8344e4a8fa7e0e74f9bbb5f99423ce
Alexandra Wittmann <alex@super-code.de>
2024-06-24 11:01:49 +0200

    feat: add countries with deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/vite-deploy/'
+  base: '/vite-deploy/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        subpage1: resolve(__dirname, 'src/countries/countries.html')
+      }
+    }
+  }
 });
